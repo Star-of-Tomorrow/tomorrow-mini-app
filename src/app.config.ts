d1bb2764453981a0327c5b4afab0ee471935f8c2
Dov Yih <yidafu@youzan.com>
2021-11-03 14:03:48 +0800

    feat: 发布瞬间页面 UI 完成

--- src/app.config.ts
+++ src/app.config.ts
@@ -2,15 +2,7 @@ import { Config } from '@tarojs/taro';
 
 import { useGlobalIconFont } from './components/iconfont/helper';
 const appConfig: Config = {
-  pages: [
-    "pages/instantaneous/index",
-    "pages/activity/index",
-    "pages/user/index",
-    "pages/institution/index",
-    "pages/my-like/index",
-    "pages/my-instantaneous/index",
-    "pages/activity-detail/index",
-  ],
+  pages: ["pages/create-instantaneous/index", "pages/instantaneous/index", "pages/activity/index", "pages/user/index", "pages/institution/index", "pages/my-like/index", "pages/my-instantaneous/index", "pages/activity-detail/index"],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#ff840a',
